fd6fb013c29738a8ebf2097f788f53de24e7c75f
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-02-21 23:58:19 +0500

    Update task4.cpp

--- lab04/task4.cpp
+++ lab04/task4.cpp
@@ -1,1 +1,21 @@
+void rentCar(int days) {
+        if (!isAvailable) {
+            cout << "Car is already rented and unavailable.\n";
+            return;
+        }
 
+        double rentalCost = rentalPrice * days;
+        this->totalRevenue += rentalCost;
+        isAvailable = false;
+
+        cout << "Car rented successfully for " << days << " days.\n";
+        cout << "Total rental cost: $" << rentalCost << "\n";
+    }
+
+    void displayCarInfo()  {
+       cout << "Brand: " << brand << "\nModel: " << model
+                  << "\nRental Price per day: $" << rentalPrice
+                  << "\nAvailability: " << (isAvailable ? "Available" : "Rented")
+                  << "\nTotal Revenue: $" << totalRevenue << "\n";
+    }
+};
