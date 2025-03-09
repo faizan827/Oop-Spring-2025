6b55433337288872da4bba095824dc759962b3ec
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-03-10 04:44:38 +0500

    Update task1.cpp

--- lab6/task1.cpp
+++ lab6/task1.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    float salary;
+
+public:
+    Employee(string empName, float empSalary) {
+        name = empName;
+        salary = empSalary;
+    }
+
+    virtual void displayDetails() {
+        cout << "Name: " << name << endl;
+        cout << "Salary: " << salary << endl;
+    }
+};
+
+class Manager : public Employee {
+private:
+    float bonus;
+
+public:
+    Manager(string mgrName, float mgrSalary, float mgrBonus)
+        : Employee(mgrName, mgrSalary), bonus(mgrBonus) {}
+
+    void displayDetails() override {
+        Employee::displayDetails();
+        cout << "Bonus: " << bonus << endl;
+    }
+};
+
+int main() {
+    string name;
+    float salary, bonus;
+
+    cout << "Enter Manager's Name: ";
+    getline(cin, name);
+    cout << "Enter Salary: ";
+    cin >> salary;
+    cout << "Enter Bonus: ";
+    cin >> bonus;
+
+    Manager mgr(name, salary, bonus);
+
+    cout << "\nManager Details:" << endl;
+    mgr.displayDetails();
+
+    return 0;
+}
 
