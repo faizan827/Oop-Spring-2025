d9a0a82e9f4d9243f2ef23a4c397ba2b1c71d81f
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-03-28 23:15:42 +0500

    Update task3.cpp

--- lab 07 /task3.cpp
+++ lab 07 /task3.cpp
@@ -1,1 +1,47 @@
+class Ticket {
+public:
+    int ticketID;
+    string passengerName;
+    float price;
+    string date;
+    string destination;
+    void reserve() {}
+    void cancel() {}
+    virtual void displayTicketInfo() {}
+};
+
+class FlightTicket : public Ticket {
+public:
+    string airlineName;
+    string flightNumber;
+    string seatClass;
+    void displayTicketInfo() {
+        cout << "Flight - Airline: " << airlineName << ", Seat Class: " << seatClass << endl;
+    }
+};
+
+class TrainTicket : public Ticket {
+public:
+    string trainNumber;
+    string coachType;
+    string departureTime;
+    void reserve() {}
+};
+
+class BusTicket : public Ticket {
+public:
+    string busCompany;
+    int seatNumber;
+    void cancel() {}
+};
+
+class ConcertTicket : public Ticket {
+public:
+    string artistName;
+    string venue;
+    string seatType;
+    void displayTicketInfo() {
+        cout << "Concert - Artist: " << artistName << ", Venue: " << venue << endl;
+    }
+};
 
