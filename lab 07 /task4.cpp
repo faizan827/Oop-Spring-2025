c34fb0cb06bbdb2f918f4eb0787ad3be5ffa9fd7
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-03-28 23:17:06 +0500

    Update task4.cpp

--- lab 07 /task4.cpp
+++ lab 07 /task4.cpp
@@ -1,1 +1,40 @@
+class Person {
+public:
+    string name;
+    int age;
+    string contactNumber;
+    string address;
+    virtual void displayInfo() {}
+    virtual void updateInfo() {}
+};
+
+class Patient : public Person {
+public:
+    int patientID;
+    string medicalHistory;
+    string doctorAssigned;
+    void displayInfo() {}
+};
+
+class Doctor : public Person {
+public:
+    string specialization;
+    float consultationFee;
+    string patientsList;
+    void displayInfo() {}
+};
+
+class Nurse : public Person {
+public:
+    string assignedWard;
+    string shiftTimings;
+    void displayInfo() {}
+};
+
+class Administrator : public Person {
+public:
+    string department;
+    float salary;
+    void updateInfo() {}
+};
 
