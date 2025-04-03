d4509809bd94e428d18091ade7a3c7db54d81b6f
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-04-03 12:33:51 +0500

    Update task1.cpp

--- lab05/task1.cpp
+++ lab05/task1.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+class Apartment {
+private:
+    int apartmentID;
+    string address;
+    string* OwnerName;
+
+public:
+    Apartment(int id, string address, string name)
+        : apartmentID(id), address(address), OwnerName(new string(name)) {}
+
+    Apartment(Apartment& other) {
+        apartmentID = other.apartmentID;
+        address = other.address;
+        OwnerName = new string(*(other.OwnerName));
+    }
+
+    void displayDetails() const {
+        cout << "Apartment ID: " << apartmentID << endl;
+        cout << "Address: " << address << endl;
+        cout << "Owner's Name: " << *OwnerName << endl;
+    }
+
+    ~Apartment() {
+        delete OwnerName;
+    } //to delete the dynamic variable
+};
+
+int main() {
+
+    Apartment SaimaClassic(538, "Johar, Karachi", "Jahanzaib");
+    SaimaClassic.displayDetails();
+
+    Apartment SaimaClassiclite(SaimaClassic);
+    cout << "\nCopied Apartment Details: " << endl;
+    SaimaClassic.displayDetails();
+
+    return 0;
+}
