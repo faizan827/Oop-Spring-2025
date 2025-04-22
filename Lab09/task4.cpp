c845a4b10f91ce702ac0f132a0bcea4d8a6182a4
faizan827 <muhammadfaizan.nara24@nixorcollege.edu.pk>
2025-04-22 23:48:11 +0500

    Update task4.cpp

--- Lab09/task4.cpp
+++ Lab09/task4.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <string>
+
+class WeatherSensor {
+public:
+    virtual void readData() = 0;
+    virtual void displayReport() = 0;
+    virtual ~WeatherSensor() {}
+};
+
+class Thermometer : public WeatherSensor {
+private:
+    float temperature;
+public:
+    void readData() override {
+        temperature = 25.5f;
+    }
+    void displayReport() override {
+        std::cout << "Thermometer Reading: " << temperature << " °C" << std::endl;
+    }
+};
+
+class Barometer : public WeatherSensor {
+private:
+    float pressure;
+public:
+    void readData() override {
+        pressure = 1013.25f;
+    }
+    void displayReport() override {
+        std::cout << "Barometer Reading: " << pressure << " hPa" << std::endl;
+    }
+};
+
+int main() {
+    WeatherSensor* sensor1 = new Thermometer();
+    WeatherSensor* sensor2 = new Barometer();
+
+    sensor1->readData();
+    sensor1->displayReport();
+
+    sensor2->readData();
+    sensor2->displayReport();
+
+    delete sensor1;
+    delete sensor2;
+
+    return 0;
+}
 
